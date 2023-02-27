4f53cf7e4d3adf771a74e7438281944d6616af86
Ahmed3mad <3umduck@gmail.com>
2023-02-27 11:58:24 +0200

    frst

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,13 @@ void main() {
             if (Platform.isAndroid) {
               urlString = 'https://play.google.com/store/apps/details?id=com.ummahshop.app';
             } else if (Platform.isIOS) {
-              final info = await DeviceInfoPlugin().iosInfo.then((info) => info.systemVersion);
-              print('$info');
+              final info = await DeviceInfoPlugin().iosInfo.then((IosDeviceInfo info) => info.systemVersion);
+              final version = int.tryParse(!info.contains('.') ? info : info.split('.').first);
+              if (version >= 11) {
+                urlString = 'itms-apps://itunes.apple.com/xy/app/foo/id%@1614235293';
+              } else {
+                urlString = 'itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=%@1614235293';
+              }
               // urlString = 'https://play.google.com/store/apps/details?id=com.ummahshop.app';
             }
             if (urlString != null) {
